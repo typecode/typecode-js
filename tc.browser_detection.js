e73f7a6dd9f85f5615232e5a740a6cb22d3f234c
Andrew Mahon (Type/Code) <andrew@typeslashcode.com>
2011-08-09 13:27:34 -0400

    added browser_detection and favicon

--- tc.browser_detection.js
+++ tc.browser_detection.js
@@ -26,59 +26,24 @@
 	
 	var NI = window.NI;
 	    
-	function Favicon(options) {
+	function BrowserDetection(options) {
 		var o;
 		
 		o = $.extend({
-			fps:5
+			addClassToBody:true
 		}, options);
 		
-		internal = {
-			interval:0.0
+		elements = {
+			body:$('body')
 		}
 		
 		function init(){
-			internal.interval = 1000/o.fps;
-		}
-		
-		var elements = {
-			head:document.getElementsByTagName("head")[0]
-		};
-		
-		//following is from http://softwareas.com/dynamic-favicons
-		var functions = {
-			change: function(iconURL) {
-				if (arguments.length==2) {
-					document.title = optionalDocTitle;
-				}
-				this.addLink(iconURL, "icon");
-				this.addLink(iconURL, "shortcut icon");
-			},
-			addLink: function(iconURL, relValue) {
-				var link = document.createElement("link");
-				link.type = "image/x-icon";
-				link.rel = relValue;
-				link.href = iconURL;
-				this.removeLinkIfExists(relValue);
-				elements.head.appendChild(link);
-			},
-			removeLinkIfExists: function(relValue) {
-				var links = elements.head.getElementsByTagName("link");
-				for (var i=0; i<links .length; i++) {
-					var link = links[i];
-					if (link.type=="image/x-icon" && link.rel==relValue) {
-						elements.head.removeChild(link);
-						return; // Assuming only one match at most.
-					}
+			if (navigator.userAgent.match(/iPad/i) != null) {
+				if(o.addClassToBody){
+					elements.body.addClass('broswer-ipad');
 				}
 			}
-		};
-		
-		
-		//Public
-		this.setFavicon = function(path){
-			functions.change(path);
-		};
+		}
 		
 		init();
 		return this;
@@ -86,6 +51,6 @@
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 		
-	NI.Favicon = Favicon;
+	NI.BrowserDetection = BrowserDetection;
 	
 }(this, this.jQuery));
